fbac5da3707fd578ee422780451f1fa9e9b5f6e3
kahmnkk <kyuhh1214@naver.com>
2024-10-17 00:23:25 +0900

    add. echo server

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"main/internal/bs"
+	"net"
+)
+
+func handler(c net.Conn) {
+	buf := make([]byte, 10)
+	for {
+		n, err := c.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed by client: ", c.RemoteAddr().String())
+			}
+			fmt.Println("Failed to receive data: ", err)
+			break
+		}
+
+		if n > 0 {
+			fmt.Println(bs.BytesToString(buf[:n]))
+			c.Write(buf[:n])
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	l, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Failed to Listen: ", err)
+	}
+	defer l.Close()
+
+	fmt.Println("server running...")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to Accept: ", err)
+			continue
+		}
+
+		go handler(conn)
+	}
 }
---
+++ test/dial_test.go
@@ -0,0 +1,76 @@
+package test
+
+import (
+	"io"
+	"main/internal/bs"
+	"net"
+	"testing"
+)
+
+func TestDial(t *testing.T) {
+	// 소켓 bind + listen
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		defer func() {
+			done <- struct{}{}
+		}()
+
+		for {
+			// 연결 수립
+			conn, err := listener.Accept()
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			// Handler. 고루틴으로 각 연결 처리
+			go func(c net.Conn) {
+				defer func() {
+					c.Close()
+					done <- struct{}{}
+				}()
+
+				buf := make([]byte, 10)
+				for {
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							t.Error(err)
+						}
+						return
+					}
+
+					t.Logf("received: %q", buf[:n])
+				}
+			}(conn)
+		}
+	}()
+
+	t.Log(listener.Addr().String())
+
+	// 클라이언트측 연결
+	conn, err := net.Dial("tcp", listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = conn.Write([]byte(bs.StringToBytes("Hello World!")))
+	if err != nil {
+		if nErr, ok := err.(net.Error); ok && nErr.Temporary() {
+			t.Logf("temporary error: %v", nErr)
+		}
+		t.Error(err)
+	}
+
+	// 연결 종료
+	conn.Close()
+	<-done
+
+	listener.Close()
+	<-done
+}
